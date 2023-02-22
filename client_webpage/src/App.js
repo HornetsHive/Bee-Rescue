4786af6b3020052c5b843b2d36d29249af34b235
Matthew <matsmt@tuta.io>
2023-02-21 17:45:15 -0800

    webpage info and formatting

--- client_webpage/src/App.js
+++ client_webpage/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Button, Pane, Text, Paragraph, Heading, SelectField, TextInputField, majorScale, toaster } from 'evergreen-ui';
+import { Button, Pane, Text, Paragraph, Heading, SelectField, TextInputField, majorScale, toaster, UnorderedList, ListItem } from 'evergreen-ui';
+import './fonts.css';
 import './App.css';
 import Axios from 'axios';
 
@@ -113,8 +114,20 @@ function App(){
     return !Object.values(newErrors).every(error => error === '');
   };
 
-  return (
+  return (    
     <div className="App">
+
+      {/*Header*/}
+      <div className="header">
+        <img src="path/to/logo.png" alt="Bee Rescue logo" />
+        <nav>
+          <ul>
+            <li><a href="#">Report a Swarm</a></li>
+            <li><a href="#">Sacramento Area Beekeepers Association</a></li>
+            <li><a href="#">About us</a></li>
+          </ul>
+        </nav>
+      </div>
         <Pane elevation={1} flexDirection="row">
           <Pane float="left" marginLeft={majorScale(4)} width="45%" flexDirection="column">
 
@@ -124,20 +137,141 @@ function App(){
               margin={majorScale(2)}
               marginTop={majorScale(6)}
             >
-              <Heading size="850">Bee Rescue - Hive Reporting</Heading>
+              <Heading fontFamily="Comfortaa-Bold" size="900">Bee Rescue - Hive Reporting</Heading>
+              <Heading paddingTop={majorScale(2)}>Welcome to the Bee Rescue Swarm reporting tool</Heading>
+              <Paragraph
+                fontFamily="Louis-George-Cafe" 
+                margin={majorScale(1)} 
+                size={500} 
+                textAlign="left"
+              >
+                Bee Rescue is an application developed in partnership with the Sacramento Area Beekeepers
+                Assocation (SABA) to facilitate the fast, safe, and humane removal
+                of bees from occupied areas. By filling out the attached form, local beekeepers in your area will be notified
+                and will contact you to make arrangements to relocate the bees to a safe area.
+              </Paragraph>
             </Pane>
 
-            {/*Text*/}
-            <Pane margin={majorScale(2)} marginTop={majorScale(6)}>
-              <Text>Why do bees swarm?</Text>
-              <Paragraph margin={majorScale(1)} size={500} textAlign="left">
-              When bees are swarming they are generally docile. They will merely find a suitable spot to gather, as a temporary measure, while they send out scout bees to find a more permanent location as a new home. They are not too choosy about where this temporary spot will be, it could be in a tree/bush, on a clothesline, a fence, a bicycle, anywhere that they can land to form a cluster to protect their queen.  If the honey bees seem to have made a home already, such as, in a tree or inside a structure (wall, roof, etc) they are an established colony. Removing an established colony is a Trap Out (TO) or Cut Out (CO) situation.{"\n"}{"\n"}
+            {/* ---------------------------Info--------------------------------*/}
+            <Pane 
+              elevation='1' 
+              margin={majorScale(2)} 
+              marginTop={majorScale(3)}
+              padding={majorScale(2)} 
+            >
+              <Heading fontFamily="Comfortaa-Bold">Why do bees swarm?</Heading>
+
+              <Paragraph 
+                fontFamily="Louis-George-Cafe" 
+                margin={majorScale(1)} 
+                size={500} 
+                textAlign="left"
+              >
+              When bees are swarming they are generally docile. They will temporarily find a suitable 
+              spot to gather while they send out scout bees to find a more permanent location as a new home.
+              They are not too choosy about where this temporary spot will be, it can be
+              anywhere that they can form a cluster to protect their queen. If the honey bees seem 
+              to have made a permanent home already, such as in a tree or inside a structure (wall, roof, etc) 
+              they are an established colony. Removing an established colony is a Trap Out (TO) or Cut Out (CO) situation.
               </Paragraph>
 
             </Pane>
+
+            <Pane 
+              elevation='1' 
+              margin={majorScale(2)} 
+              marginTop={majorScale(3)} 
+              padding={majorScale(2)} 
+            >
+              <Heading fontFamily="Comfortaa-Bold">Make Observations</Heading>
+
+              <Paragraph 
+                fontFamily="Louis-George-Cafe" 
+                margin={majorScale(1)} 
+                size={500} 
+                textAlign="left"
+              >
+                Please have the following information ready before you submit your report. <br></br>Notes to make:<br></br>
+                <UnorderedList padding='0.5em' paddingTop='0'>
+                  <ListItem margin='-0.1em'>Are they honey bees or other similar insects?</ListItem>
+                  <ListItem margin='-0.1em'>Where are the bees?</ListItem>
+                  <ListItem margin='-0.1em'>How high above the ground?</ListItem>
+                  <ListItem margin='-0.1em'>What size is the swarm? (tennis ball, basketball, etc)</ListItem>
+                  <ListItem margin='-0.1em'>How long it has been in this spot?</ListItem>
+                  <ListItem margin='-0.1em'>Take photos to help indicate cluster size, location details, site considerations (power lines, slopes, obstructions, etc).</ListItem>
+                </UnorderedList>
+
+                Terminlogy:<br></br>
+                Bees that have recently settled and clustered together are a swarm. 
+                If the bees have been living there for a while, it is an established colony
+              </Paragraph>
+            </Pane>
+
+            <Pane 
+              elevation='1' 
+              margin={majorScale(2)} 
+              marginTop={majorScale(3)}
+              padding={majorScale(2)} 
+            >
+              <Heading fontFamily="Comfortaa-Bold">Property Rights/Liabilites</Heading>
+
+              <Paragraph 
+                fontFamily="Louis-George-Cafe" 
+                margin={majorScale(1)} 
+                size={500} 
+                textAlign="left"
+              >
+              If the bees are not on your property, please send this page to the property owner
+              for them to submit their own bee rescue report. Bee Rescue is a tool to facilitate quick
+              communication between property owners and beekeepers, any arrangements made are between you and
+              the beekeeper that agrees to do the collection. Bee Rescue does not employ, recommend, or endorse any
+              beekeeper’s skill, training or expertise. Please check with individual beekeepers for their qualifications.
+              </Paragraph>
+            </Pane>
+
+            <Pane 
+              elevation='1' 
+              margin={majorScale(2)} 
+              marginTop={majorScale(3)}
+              padding={majorScale(2)} 
+            >
+              <Heading fontFamily="Comfortaa-Bold">Safe Distance</Heading>
+
+              <Paragraph 
+                fontFamily="Louis-George-Cafe" 
+                margin={majorScale(1)} 
+                size={500} 
+                textAlign="left"
+              >
+              Sit back, watch and wait for the beekeeper to arrive.
+              Any beekeeper will tell you that there is something very magical about a swarm.
+              There is an electric feeling in the air, as the bees swirl round before clustering in a ball.
+              Watching a swarm being collected is an experience that you will not soon forget!
+              </Paragraph>
+            </Pane>
+
+            <Pane 
+              elevation='1' 
+              margin={majorScale(2)} 
+              marginTop={majorScale(3)}
+              padding={majorScale(2)} 
+            >
+              <Heading fontFamily="Comfortaa-Bold">Tell Others or Contribute</Heading>
+
+              <Paragraph 
+                fontFamily="Louis-George-Cafe" 
+                margin={majorScale(1)} 
+                size={500} 
+                textAlign="left"
+              >
+              Share your positive experience with Bee Rescue and beekeepers with others! Bees often get negative press
+              and we want to make sure that more people learn more about these amazing insects. Please consider
+              donating to the Sacramento Area Beekeepers Association (SABA)
+              </Paragraph>
+            </Pane>
           </Pane>
 
-          {/*Form*/}
+          {/* ---------------------------Form--------------------------------*/}
           <Pane
             elevation={1}
             width="33%"
@@ -146,9 +280,7 @@ function App(){
             marginRight={majorScale(16)}
             float="right"
             align="center"
-            backgroundColor="white"
             flexDirection="row"
-            style={{position:"Fixed", top:0, right:0}}
           >
             <Heading margin={majorScale(1)} size="600">Submit a new Bee Rescue report</Heading>
             <div className = "form">
