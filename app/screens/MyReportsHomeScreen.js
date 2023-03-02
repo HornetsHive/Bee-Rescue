dfc87cdc46b21e525624743a6479dcf0045a0cdd
John <johnwishek@yahoo.com>
2023-03-02 15:28:55 -0800

    implemented MyReportsHome Screen

--- app/screens/MyReportsHomeScreen.js
+++ app/screens/MyReportsHomeScreen.js
@@ -13,14 +13,14 @@ import {
   TouchableOpacity,
 } from "react-native";
 
-//import Report from "../components/report";
-
-export default function HomeScreen({ navigation }) {
-  const [reports, setReports] = React.useState([]);
-  const reportArray = new Array();
+export default function MyReportsHomeScreen({ navigation }) {
+  const [claimedreports, setClaimedReports] = React.useState([]);
+  const [completedreports, setCompletedReports] = React.useState([]);
+  const clamReportArray = new Array();
   const dateArray = new Array();
   const timeArray = new Array();
   const dateTime = new Array();
+  const compReportArray = new Array();
   const [loaded] = useFonts({
     Comfortaa: require("../assets/fonts/Comfortaa-Regular.ttf"),
     RoundSerif: require("../assets/fonts/rounded-sans-serif.ttf"),
@@ -28,45 +28,37 @@ export default function HomeScreen({ navigation }) {
 
   var location;
   var date;
-  
-  //fetching info from database to display
-  const fetchReports = async () => {
-    const res = await Axios
-      //10.0.2.2 is a general IP address for the emulator
-      .get("http://10.0.2.2:3001/api/bk_appReports")
+
+  const showClaimedReports = async () => {
+    //needs the beekeeper
+    const res = await Axios.get("http://10.0.2.2:3001/api/bk_claimedReports", {
+        params: { bk_id: "1"}
+    })
       .then((res) => {
-        setReports(res.data);
+        setClaimedReports(res.data);
       })
       .catch(function (error) {
         if (error.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
           console.log(error.response.data);
           console.log(error.response.status);
           console.log(error.response.headers);
         } else if (error.request) {
-          // The request was made but no response was received
-          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-          // http.ClientRequest in node.js
           console.log(error.request);
         } else {
-          //Something happened in setting up the request that triggered an Error
-          console.log("Error", error.message);
+          console.log("Error: ", error.message);
         }
         console.log(error.config);
       });
     return res;
   };
-  
   var i = 0;
-  reports.map((reports) => {
-    reportArray[i] = [reports.address, ", ", reports.city];
-    dateArray[i] = [reports.rdate];
+  claimedreports.map((claimedreports) => {
+    clamReportArray[i] = [claimedreports.address, ", ", claimedreports.city];
+    dateArray[i] = [claimedreports.rdate];
     i++;
   });
 
-  //dateTime conversions for homescreen display
-  for (var j = 0; j < reportArray.length; j++) {
+  for (var j = 0; j < claimedreports.length; j++) {
     //stringinfy the array with the date and time
     dateArray[j] = JSON.stringify(dateArray[j]);
 
@@ -105,20 +97,19 @@ export default function HomeScreen({ navigation }) {
     timeArray[j] = [hour, ":", minute, " ", am_pm];
 
     //put date and time into one array to use for display
-    dateTime[j] = [dateArray[j], " at ", timeArray[j]];
+    dateTime[j] = [dateArray[j], " at", timeArray[j]];
   }
 
   if (!loaded) {
     return null;
   }
-
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <TouchableOpacity
           onPress={() =>
-            navigation.navigate("AccountScreen", {
-              screen: "AccountScreen",
+            navigation.navigate("HomeScreen", {
+              screen: "HomeScreen",
             })
           }
         >
@@ -131,95 +122,33 @@ export default function HomeScreen({ navigation }) {
             }}
           />
         </TouchableOpacity>
-        <Text style={{ fontFamily: "Comfortaa", fontSize: 16 }}>
-          Location: Orangevale
+        <Text style={{ fontFamily: "Comfortaa", fontSize: 17, width: 125 }}>
+          My Reports
         </Text>
-        <TouchableOpacity
-          onPress={() =>
-            navigation.navigate("SettingsScreen", {
-              screen: "SettingsScreen",
-            })
-          }
-        >
-          <Image
-            source={require("../assets/bell.png")}
-            style={{
-              resizeMode: "contain",
-              height: 30,
-              width: 30,
-            }}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() =>
-            navigation.navigate("MyReportsHomeScreen", {
-              screen: "MyReportsHomeScreen",
-            })
-          }
-        >
-          <Image
-            source={require("../assets/myReportButton.png")}
-            style={{
-              resizeMode: "contain",
-              height: 30,
-              width: 30,
-            }}
-          />
-        </TouchableOpacity>
-      </View>
-      
-
-      <View style={styles.middle}>
-        <Image
-          source={require("../assets/map.png")}
-          style={{
-            alignSelf: "center",
-            resizeMode: "contain",
-            height: 180,
-          }}
-        />
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-          }}
-        >
-          <TouchableOpacity>
-            <Image
-              source={require("../assets/sort.png")}
-              style={{
-                resizeMode: "contain",
-                margin: 10,
-                height: 30,
-                width: 30,
-              }}
-            ></Image>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.textContainer}>
-            <Text style={styles.textBox}>Change location</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => fetchReports()}>
+        <TouchableOpacity 
+        onPress={() => showClaimedReports()}>
             <Image
               source={require("../assets/refresh.png")}
               style={{
                 resizeMode: "contain",
-                margin: 10,
                 height: 30,
                 width: 30,
               }}
             ></Image>
-          </TouchableOpacity>
-        </View>
+            </TouchableOpacity>
+      </View>
+
+      <View style={styles.middle}>
         <ScrollView>
-{/*---------------Start of scroll---------------------------------*/}
+          {/*---------------Start of scroll---------------------------------*/}
           <TouchableOpacity
             onPress={() => {
-              var specificReport = reports.filter(obj => {
-                return obj.r_id === reports[0].r_id
+              var specificReport = claimedreports.filter((obj) => {
+                return obj.r_id === claimedreports[0].r_id;
               })[0];
 
-              navigation.navigate("ReportInfoScreen", {
-                screen: "ReportInfoScreen",
+              navigation.navigate("MyReportsScreen", {
+                screen: "MyReportsScreen",
                 report: specificReport,
               });
             }}
@@ -227,7 +156,7 @@ export default function HomeScreen({ navigation }) {
             <View style={styles.task}>
               <View style={styles.taskText}>
                 <Text value={{}} style={{ fontSize: 16 }}>
-                  A swarm has been reported at {reportArray[0]}
+                  A swarm has been reported at {clamReportArray[0]}
                 </Text>
                 <TouchableOpacity>
                   <Image
@@ -242,12 +171,12 @@ export default function HomeScreen({ navigation }) {
 
           <TouchableOpacity
             onPress={() => {
-              var specificReport = reports.filter(obj => {
-                return obj.r_id === reports[1].r_id
+              var specificReport = claimedreports.filter((obj) => {
+                return obj.r_id === claimedreports[1].r_id;
               })[0];
 
-              navigation.navigate("ReportInfoScreen", {
-                screen: "ReportInfoScreen",
+              navigation.navigate("MyReportsScreen", {
+                screen: "MyReportsScreen",
                 report: specificReport,
               });
             }}
@@ -255,7 +184,7 @@ export default function HomeScreen({ navigation }) {
             <View style={styles.task}>
               <View style={styles.taskText}>
                 <Text style={{ fontSize: 16 }}>
-                  A swarm has been reported at {reportArray[1]}
+                  A swarm has been reported at {clamReportArray[1]}
                 </Text>
                 <TouchableOpacity>
                   <Image
@@ -268,54 +197,56 @@ export default function HomeScreen({ navigation }) {
             </View>
           </TouchableOpacity>
 
-          {/*<TouchableOpacity
-            onPress={() => {
-              var specificReport = reports.filter(obj => {
-                return obj.r_id === reports[2].r_id
-              })[0];
+          <TouchableOpacity
+                onPress={() => {
+                  var specificReport = claimedreports.filter(obj => {
+                    return obj.r_id === claimedreports[2].r_id
+                  })[0];
+    
+                  navigation.navigate("MyReportsScreen", {
+                    screen: "MyReportsScreen",
+                    report: specificReport,
+                  });
+                }}
+              >
+                <View style={styles.task}>
+                  <View style={styles.taskText}>
+                    <Text style={{ fontSize: 16 }}>
+                      A swarm has been reported at {clamReportArray[2]}
+                    </Text>
+                    <TouchableOpacity>
+                      <Image
+                        source={require("../assets/x.png")}
+                        style={styles.xButton}
+                      ></Image>
+                    </TouchableOpacity>
+                  </View>
+                  <Text>{dateTime[2]}</Text>
+                </View>
+              </TouchableOpacity>
 
-              navigation.navigate("ReportInfoScreen", {
-                screen: "ReportInfoScreen",
-                report: specificReport,
-              });
-            }}
-          >
-            <View style={styles.task}>
-              <View style={styles.taskText}>
-                <Text style={{ fontSize: 16 }}>
-                  A swarm has been reported at {reportArray[2]}
-                </Text>
-                <TouchableOpacity>
-                  <Image
-                    source={require("../assets/x.png")}
-                    style={styles.xButton}
-                  ></Image>
-                </TouchableOpacity>
-              </View>
-              <Text>{dateTime[2]}</Text>
-            </View>
-          </TouchableOpacity>*/}
-          
           {/*<TouchableOpacity><Report /></TouchableOpacity>*/}
 
-          {reportArray.map((report) => <TouchableOpacity>
-            <View style={styles.task}>
-              <View style={styles.taskText}>
-                <Text style={{ fontSize: 16 }} location={report.address}>
-                  A swarm has been reported at {location}
-                </Text>
-                <TouchableOpacity>
-                  <Image
-                    source={require("../assets/x.png")}
-                    style={styles.xButton}
-                  ></Image>
-                </TouchableOpacity>
+          {clamReportArray.map((report) => (
+            <TouchableOpacity>
+              <View style={styles.task}>
+                <View style={styles.taskText}>
+                  <Text style={{ fontSize: 16 }} location={report.address}>
+                    A swarm has been reported at {location}
+                  </Text>
+                  <TouchableOpacity>
+                    <Image
+                      source={require("../assets/x.png")}
+                      style={styles.xButton}
+                    ></Image>
+                  </TouchableOpacity>
+                </View>
+                <Text date={report.dateTime}>{date}</Text>
               </View>
-              <Text date={report.dateTime}>{date}</Text>
-            </View>
-          </TouchableOpacity>)}
+            </TouchableOpacity>
+          ))}
 
-{/*--------------End of scroll-------------------------------*/}
+          {/*--------------End of scroll-------------------------------*/}
         </ScrollView>
       </View>
 
