876b7b94ca2467892a61096c98b969b4ccd68886
Kiana <kianabrunberg@csus.edu>
2023-03-29 12:38:55 -0700

    test notification set up. triggers afer 10 seconds

--- app/PushController.js
+++ app/PushController.js
@@ -1,1 +1,39 @@
 //responsible for phing notifications to users
+import * as Notifications from "expo-notifications";
+import {
+  getExpoPushTokenAsync,
+  scheduleNotificationAsync,
+} from "expo-notifications";
+
+const registerForPushNotifications = async () => {
+  try {
+    const permission = await Notifications.getPermissionsAsync();
+    if (!permission.granted) return;
+    const token = await getExpoPushTokenAsync();
+    return token;
+  } catch (error) {
+    console.log("Error getting a token", error);
+  }
+};
+
+const createSender = async () => {
+  // Get Token
+  const token = await registerForPushNotifications();
+
+  // function for sending push notifications
+  const sendPushNotification = async (message) => {
+    scheduleNotificationAsync({
+      content: {
+        title: "Notification test from Bee Rescue",
+        body: "New report!",
+      },
+      trigger: {
+        seconds: 10,
+      },
+    });
+  };
+
+  return sendPushNotification;
+};
+
+export default createSender;
